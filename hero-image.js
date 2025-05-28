fb1095119603850caa3e50e2f1b5ecad41b90a4d
Ironicpanda45 <88987033+Ironicpanda45@users.noreply.github.com>
2025-05-27 22:12:11 -0600

    Update hero-image.js

--- hero-image.js
+++ hero-image.js
@@ -1,13 +1,13 @@
     const heroImage = document.querySelector('.hero-image');
     const heroImages = [
-        'Img/Hero/Hero1.png',
-        'Img/Hero/Hero2.png',
-        'Img/Hero/Hero3.png',
-        'Img/Hero/Hero4.png',
-        'Img/Hero/Hero5.png',
-        'Img/Hero/Hero6.png',
-        'Img/Hero/Hero7.png',
-        'Img/Hero/Hero8.png'
+        'Img/hero/hero1.png',
+        'Img/hero/hero2.png',
+        'Img/hero/hero3.png',
+        'Img/hero/hero4.png',
+        'Img/hero/hero5.png',
+        'Img/hero/hero6.png',
+        'Img/hero/hero7.png',
+        'Img/hero/hero8.png'
     ];
     let currentIndex = 0;
 
